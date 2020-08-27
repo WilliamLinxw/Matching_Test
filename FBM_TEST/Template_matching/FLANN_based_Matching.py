2f84e153b22927f9e78ee25df1e7b903f06ecac7
CaesarLinxw <xiaowei.lin1997@gmail.com>
2020-08-27 10:08:41 +0800

    Test updated

--- FBM_TEST/Template_matching/FLANN_based_Matching.py
+++ FBM_TEST/Template_matching/FLANN_based_Matching.py
@@ -1,21 +1,35 @@
-import cv2 as cv
+import numpy as np
+import cv2
 from matplotlib import pyplot as plt
-queryImage=cv.imread("template_adjusted.jpg",0)
-trainingImage=cv.imread("target.jpg",0)#读取要匹配的灰度照片
-sift=cv.xfeatures2d.SIFT_create()#创建sift检测器
-kp1, des1 = sift.detectAndCompute(queryImage,None)
-kp2, des2 = sift.detectAndCompute(trainingImage,None)
-#设置Flannde参数
-FLANN_INDEX_KDTREE=0
-indexParams=dict(algorithm=FLANN_INDEX_KDTREE,trees=5)
-searchParams= dict(checks=50)
-flann=cv.FlannBasedMatcher(indexParams,searchParams)
-matches=flann.knnMatch(des1,des2,k=2)
-#设置好初始匹配值
-matchesMask=[[0,0] for i in range (len(matches))]
-for i, (m,n) in enumerate(matches):
-	if m.distance< 0.5*n.distance: #舍弃小于0.5的匹配结果
-		matchesMask[i]=[1,0]
-drawParams=dict(matchColor=(0,0,255),singlePointColor=(255,0,0),matchesMask=matchesMask,flags=0) #给特征点和匹配的线定义颜色
-resultimage=cv.drawMatchesKnn(queryImage,kp1,trainingImage,kp2,matches,None,**drawParams) #画出匹配的结果
-plt.imshow(resultimage,),plt.show()
+
+queryImage = cv2.imread('Template_target/chessboard_template.jpg', 0)
+trainingImage = cv2.imread('Template_target/chessboard_target.png', 0)
+
+# create SIFT and detect/compute
+sift = cv2.xfeatures2d.SIFT_create()
+kp1, des1 = sift.detectAndCompute(queryImage, None)
+kp2, des2 = sift.detectAndCompute(trainingImage, None)
+
+# FLANN matcher parameters
+FLANN_INDEX_KDTREE = 0
+indexParams = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+searchParams = dict(checks = 50) # or pass empty dictionary
+
+flann = cv2.FlannBasedMatcher(indexParams, searchParams)
+
+matches = flann.knnMatch(des1, des2, k = 2)
+
+# prepare an empty mask to draw good matches
+matchesMask = [[0, 0] for i in range(len(matches))]
+
+# David G. Lowe's ratio test, populate the mask
+for i, (m, n) in enumerate(matches):
+	if m.distance < 0.7 * n.distance:
+		matchesMask[i] = [1, 0]
+
+drawParams = dict(matchColor = (0, 255, 0), singlePointColor = (255, 0, 0), matchesMask = matchesMask, flags = 0)
+
+resultImage = cv2.drawMatchesKnn(queryImage, kp1, trainingImage, kp2, matches, None, **drawParams)
+
+plt.imshow(resultImage)
+plt.show()
--- FBM_TEST/Template_matching/ORB_matching.py
+++ FBM_TEST/Template_matching/ORB_matching.py
@@ -8,9 +8,15 @@ img2 = cv2.imread('Template_target/chessboard_target.png', cv2.IMREAD_GRAYSCALE)
 orb = cv2.ORB_create()
 kp1, des1 = orb.detectAndCompute(img1, None)
 kp2, des2 = orb.detectAndCompute(img2, None)
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+
+# For Brute Force matching
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+# matches = bf.match(des1, des2)
+
+# For KNN Matching
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = False)
 matches = bf.knnMatch(des1, des2, k = 2)
-matches = sorted(matches, key = lambda x : x.distance)
-img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:40], img2, flags = 2)
+# matches = sorted(matches, key = lambda x : x.distance)
+img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, img2, flags = 2)
 plt.imshow(img3)
 plt.show()
